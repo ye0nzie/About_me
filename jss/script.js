38240e14321a809f0ec8a3ee2c640d9bcb83a060
yeonzie <ivaniaa1956@gmail.com>
2026-06-18 11:17:56 +0700

    18 june

--- jss/script.js
+++ jss/script.js
@@ -1,4 +1,4 @@
-// 1. Logika Animasi Header (Kapsul Bubble ke Sticky Bar Datar)
+
 const header = document.getElementById('main-header');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
@@ -8,11 +8,11 @@ window.addEventListener('scroll', () => {
     }
 });
 
-// 2. Logika Intersection Observer (Memicu Efek Fade-In Slide Up)
+
 const sections = document.querySelectorAll('section');
 const observerOptions = {
     root: null,
-    threshold: 0.12 // Animasi berjalan saat 12% bagian halaman terintip
+    threshold: 0.12 
 };
 
 const sectionObserver = new IntersectionObserver((entries) => {
@@ -20,7 +20,7 @@ const sectionObserver = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('is-visible');
         } else {
-            entry.target.classList.remove('is-visible'); // Hapus ini jika ingin animasi 1x jalan saja
+            entry.target.classList.remove('is-visible'); 
         }
     });
 }, observerOptions);
